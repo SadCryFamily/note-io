ad7d43b0b5975f472ed714b627623465f34f6470
Semen Hulenko <semen.hulenko@nure.ua>
2022-07-25 23:29:30 +0300

    added Swagger2.0

--- src/main/java/com/example/firstspringapp/controller/CustomerController.java
+++ src/main/java/com/example/firstspringapp/controller/CustomerController.java
@@ -3,37 +3,45 @@ package com.example.firstspringapp.controller;
 import com.example.firstspringapp.dao.CustomerDao;
 import com.example.firstspringapp.dto.CustomerDto;
 import com.example.firstspringapp.service.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api
 @RestController
 public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
 
+    @ApiOperation("Adding new customer")
     @PostMapping("/add")
     public CustomerDto createUser(@RequestBody CustomerDao customerDao) {
         return customerService.createUser(customerDao);
     }
 
+    @ApiOperation("Deleting customer by id")
     @DeleteMapping("/delete/{id}")
     public CustomerDto deleteUserById(@PathVariable Long id) {
         return customerService.deleteUser(id);
     }
 
+    @ApiOperation("Get customer profile")
     @GetMapping("/profile")
     public CustomerDto getUser(@RequestParam Long id) {
         return customerService.getUser(id);
     }
 
+    @ApiOperation("Get all customers")
     @GetMapping("/statistics")
     public List<CustomerDto> getAllUsers() {
         return customerService.getAllUsers();
     }
 
+    @ApiOperation("Update customer by id and new name")
     @PutMapping("/update")
     CustomerDto updateUser(@RequestParam Long id, @RequestBody String newName) {
         return customerService.updateUser(id, newName);
--- src/main/java/com/example/firstspringapp/controller/NoteController.java
+++ src/main/java/com/example/firstspringapp/controller/NoteController.java
@@ -3,11 +3,14 @@ package com.example.firstspringapp.controller;
 import com.example.firstspringapp.dao.NoteDao;
 import com.example.firstspringapp.dto.NoteDto;
 import com.example.firstspringapp.service.NoteService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api
 @RestController
 @RequestMapping("/profile")
 public class NoteController {
@@ -15,21 +18,25 @@ public class NoteController {
     @Autowired
     private NoteService noteService;
 
+    @ApiOperation("Creating new note for customer")
     @PostMapping("/{id}/create")
     public NoteDto addNewNote(@PathVariable Long id, @RequestBody NoteDao note) {
         return noteService.addNewNote(id, note);
     }
 
+    @ApiOperation("Deleting note by id")
     @GetMapping("/{id}/note")
     public NoteDto getCurrentNote(@PathVariable Long id, @RequestParam Long noteId) {
         return noteService.getCurrentNote(id, noteId);
     }
 
+    @ApiOperation("Updating note by id")
     @PutMapping("/{id}/update")
     public NoteDto updateCurrentNote(@PathVariable Long id, @RequestParam Long noteId, @RequestBody NoteDto noteDto) {
         return noteService.updateCurrentNote(id, noteId, noteDto);
     }
 
+    @ApiOperation("Get all notes from all customers")
     @GetMapping("/{id}/all")
     public List<NoteDto> showCustomerNotes(@PathVariable Long id) {
         return noteService.showAllCustomerNotes(id);
